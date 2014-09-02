c164ced716dc3691ad6dff1d93ff9fa6108bf502
Mike Lennon <michaellennon603@yahoo.com>
2014-09-02 02:18:27 -0400

    Script Attempt 1

---
+++ Assign1Script.R
@@ -0,0 +1,5 @@
+# 1. R for Windows  version 3.1.1  
+# R Studio version  Version 0.98.1028 - © 2009-2013 RStudio, Inc.
+# 2.Postgre sql:
+#Installer version Version 9.3.5.1
+#3.I installed DMwR - Using the tail(sales) command, found that there were 411,818 lines
